554640257d7f0e87f1016391cc7ab153acbbaad9
zion <18102466666>
2018-06-07 08:16:14 +0800

    部门层级dto

---
+++ src/main/java/com/myself/dto/DeptLevelDto.java
@@ -0,0 +1,24 @@
+package com.myself.dto;
+
+import com.google.common.collect.Lists;
+import com.myself.model.SysDept;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.beans.BeanUtils;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class DeptLevelDto extends SysDept {
+
+    private List<DeptLevelDto> deptList = Lists.newArrayList();
+
+    public static DeptLevelDto adapt(SysDept dept) {
+        DeptLevelDto dto = new DeptLevelDto();
+        BeanUtils.copyProperties(dept, dto);
+        return dto;
+    }
+}
