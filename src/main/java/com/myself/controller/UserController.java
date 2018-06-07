2b00bb7d05ac2ee532b6180b6d16786adf39963a
zion <18102466666>
2018-06-07 08:14:27 +0800

    用户、部门 controller

--- src/main/java/com/myself/controller/UserController.java
+++ src/main/java/com/myself/controller/UserController.java
@@ -1,16 +1,8 @@
 package com.myself.controller;
 
-import com.myself.beans.PageQuery;
-import com.myself.beans.PageResult;
-import com.myself.common.JsonData;
-import com.myself.model.SysUser;
-import com.myself.param.UserParam;
 import com.myself.service.SysUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -19,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
-@RequestMapping("/sys/user")
 public class UserController {
 
     @Resource
@@ -46,53 +37,4 @@ public class UserController {
     public void login(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
         sysUserService.processLogin(request,response);
     }
-
-    /**
-     * 跳转没有权限页面
-     * @return
-     */
-    @RequestMapping("/noAuth.page")
-    @ResponseBody
-    public ModelAndView noAuth(){
-        return new ModelAndView("noAuth");
-    }
-
-    /**
-     * 添加用户
-     * @param param 用户输入参数
-     * @return
-     */
-    @RequestMapping("/save.json")
-    @ResponseBody
-    public JsonData save(UserParam param){
-        sysUserService.save(param);
-        return  JsonData.success();
-    }
-
-    /**
-     * 用户更新
-     * @param param
-     * @return
-     */
-    @RequestMapping("/update.json")
-    @ResponseBody
-    public JsonData updateUser(UserParam param) {
-        sysUserService.update(param);
-        return JsonData.success();
-    }
-
-    /**
-     * 分页查询
-     * @param deptId
-     * @param pageQuery
-     * @return
-     */
-    @RequestMapping("/page.json")
-    @ResponseBody
-    public JsonData page(@RequestParam("deptId") int deptId, PageQuery pageQuery) {
-        PageResult<SysUser> result = sysUserService.getPageByDeptId(deptId, pageQuery);
-        return JsonData.success(result);
-    }
-
-
 }
