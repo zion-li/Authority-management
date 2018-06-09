b2f686833dd06a934074ff831fafb39fc9446415
zion <18102466666>
2018-06-09 20:47:06 +0800

    日志记录

---
+++ src/main/java/com/myself/controller/SysLogController.java
@@ -0,0 +1,39 @@
+package com.myself.controller;
+
+import com.myself.beans.PageQuery;
+import com.myself.common.JsonData;
+import com.myself.param.SearchLogParam;
+import com.myself.service.SysLogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("/sys/log")
+public class SysLogController {
+
+    @Resource
+    private SysLogService sysLogService;
+
+    @RequestMapping("/log.page")
+    public ModelAndView page() {
+        return new ModelAndView("log");
+    }
+
+    @RequestMapping("/recover.json")
+    @ResponseBody
+    public JsonData recover(@RequestParam("id") int id) {
+        sysLogService.recover(id);
+        return JsonData.success();
+    }
+
+    @RequestMapping("/page.json")
+    @ResponseBody
+    public JsonData searchPage(SearchLogParam param, PageQuery page) {
+        return JsonData.success(sysLogService.searchPageList(param, page));
+    }
+}
