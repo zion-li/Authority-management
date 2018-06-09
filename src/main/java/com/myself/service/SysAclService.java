ea22f529cfeeb5c340673bd54444d46eb62c324d
zion <18102466666>
2018-06-09 20:58:30 +0800

    权限

--- src/main/java/com/myself/service/SysAclService.java
+++ src/main/java/com/myself/service/SysAclService.java
@@ -5,12 +5,34 @@ import com.myself.param.AclParam;
 
 import java.util.Map;
 
+/**
+ * 权限接口
+ */
 public interface SysAclService {
+    /**
+     * 保存权限
+     * @param param
+     */
     void save(AclParam param);
 
+    /**
+     * 更新
+     * @param param
+     */
     void update(AclParam param);
 
+    /**
+     * 获取权限列表
+     * @param aclModuleId
+     * @param pageQuery
+     * @return
+     */
     Object getPageByAclModuleId(Integer aclModuleId, PageQuery pageQuery);
 
-    Map getaclsByAclId(int aclId);
+    /**
+     * 获取特定权限
+     * @param aclId
+     * @return
+     */
+    Map getAclByAclId(int aclId);
 }
