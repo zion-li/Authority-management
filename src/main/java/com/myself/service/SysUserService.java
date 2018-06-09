1cf0549cc673e4524d273e6550490f2bd2f23ded
zion <18102466666>
2018-06-09 08:44:21 +0800

    用户相关接口

--- src/main/java/com/myself/service/SysUserService.java
+++ src/main/java/com/myself/service/SysUserService.java
@@ -2,6 +2,7 @@ package com.myself.service;
 
 import com.myself.beans.PageQuery;
 import com.myself.beans.PageResult;
+import com.myself.common.JsonData;
 import com.myself.model.SysUser;
 import com.myself.param.UserParam;
 
@@ -9,7 +10,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
+/**
+ * 用户接口
+ * @author zion
+ */
 public interface SysUserService {
 
     /**
@@ -39,10 +46,23 @@ public interface SysUserService {
     void update(UserParam param);
 
     /**
-     * 分页查询用户
+     * 根据部门分页查询用户
      * @param deptId
      * @param pageQuery
      * @return
      */
     PageResult<SysUser> getPageByDeptId(int deptId, PageQuery pageQuery);
+
+    /**
+     * 获取用户角色以及权限
+     * @param userId
+     * @return
+     */
+    Map<String, Object> getAclsByUserId(int userId);
+
+    /**
+     * 获取所有用户
+     * @return
+     */
+    List<SysUser> getAll();
 }
