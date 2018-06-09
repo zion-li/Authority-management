4c32a3a8db6bc4bbb0dc689c1811660373b5dec0
zion <18102466666>
2018-06-09 20:54:07 +0800

    日志记录

--- src/main/java/com/myself/dao/SysLogMapper.java
+++ src/main/java/com/myself/dao/SysLogMapper.java
@@ -1,7 +1,12 @@
 package com.myself.dao;
 
+import com.myself.beans.PageQuery;
+import com.myself.dto.SearchLogDto;
 import com.myself.model.SysLog;
 import com.myself.model.SysLogWithBLOBs;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface SysLogMapper {
     int deleteByPrimaryKey(Integer id);
@@ -17,4 +22,8 @@ public interface SysLogMapper {
     int updateByPrimaryKeyWithBLOBs(SysLogWithBLOBs record);
 
     int updateByPrimaryKey(SysLog record);
+
+    int countBySearchDto(@Param("dto") SearchLogDto dto);
+
+    List<SysLogWithBLOBs> getPageListBySearchDto(@Param("dto") SearchLogDto dto,@Param("page") PageQuery page);
 }
