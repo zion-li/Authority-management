e03ff168619a24ff93eaecd88bb47103b59e371b
zion <18102466666>
2018-06-15 20:50:46 +0800

    生成密码注释

--- src/main/java/com/myself/util/PasswordUtil.java
+++ src/main/java/com/myself/util/PasswordUtil.java
@@ -1,12 +1,18 @@
 package com.myself.util;
 
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
+/**
+ * 生成随机密码的工具类
+ */
+@Slf4j
 public class PasswordUtil {
     /**
      * 生成密码的基础英文值，去掉容易看错的
@@ -30,12 +36,13 @@ public class PasswordUtil {
         try {
             TimeUnit.MILLISECONDS.sleep(3);
         } catch (InterruptedException e) {
-            //TODO 再说
-            e.printStackTrace();
+          log.error("thread exception"+e.getMessage());
         }
         Random random = new Random(new Date().getTime());
         boolean flag = false;
+        //随机密码是8 9 10位都有可能
         int length = random.nextInt(3) + 8;
+        //生成的密码是在数字和字符之间变换的
         for (int i = 0; i < length; i++) {
             if (flag) {
                 stringBuffer.append(num[random.nextInt(num.length)]);
